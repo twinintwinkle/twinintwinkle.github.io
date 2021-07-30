7c233278093d5d82042c87a64d5f99a534f5bf04
Tiphanie <cchongtingho@gmail.com>
2021-07-30 18:12:15 +0800

    update

---
+++ js/radialgraph_spec.js
@@ -0,0 +1,165 @@
+var language = new RadialProgressChart('#cantonese', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 350,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'Cantonese';
+    }
+});
+
+var language = new RadialProgressChart('#mandarin', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 340,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'Mandarin';
+    }
+});
+
+var language = new RadialProgressChart('#english', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 290,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'English';
+    }
+});
+
+var language = new RadialProgressChart('#japanese', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 140,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'Japanese';
+    }
+});
+
+var language = new RadialProgressChart('#german', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 40,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'German';
+    }
+});
+
+
+var language = new RadialProgressChart('#html', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 300,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'HTML';
+    }
+});
+
+var language = new RadialProgressChart('#css', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 200,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'CSS';
+    }
+});
+
+var language = new RadialProgressChart('#java', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 180,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'Java';
+    }
+});
+
+var language = new RadialProgressChart('#python', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 100,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'Python';
+    }
+});
+
+var language = new RadialProgressChart('#c', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 70,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'C';
+    }
+});
+
+var language = new RadialProgressChart('#javascript', {
+    diameter: 300,
+    max: 360,
+    round: false,
+    series: [
+        {
+            value: 40,
+            color: ['#D47800', '#00FFF7']
+        }
+    ],
+    center: function (d) {
+        return 'Javascript';
+    }
+});
